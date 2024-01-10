7ebce4ba07d15daf52b836f8d372315d26e46e50
Lautaro Matas <lmatas@gmail.com>
2024-01-10 17:49:16 +0100

    Add configuration file validation

--- configcontext.py
+++ configcontext.py
@@ -4,9 +4,19 @@
 import logging
 logger = logging.getLogger()
 
+import configparser
+
 class ConfigurationContext:
 
    def __init__(self, commandLineArgs):
-      pass
+      self._commandLineArgs = commandLineArgs
+
+      if commandLineArgs.config_file is None:
+         logger.error("No configuration file specified")
+         raise Exception("No configuration file specified")
+      else:
+         config = configparser.ConfigParser()
+         self._config = config.read(commandLineArgs.config_file)
+      
         
        
