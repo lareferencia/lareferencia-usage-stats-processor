d454a433d5762303e21c52b610e3a82422601b0e
Lautaro Matas <lmatas@gmail.com>
2024-02-13 17:23:15 +0100

    Update load_year.sh

--- load_year.sh
+++ load_year.sh
@@ -4,11 +4,22 @@
 
 while getopts "s:y:" opt; do
   case $opt in
+#!/bin/bash
+
+# Usage: ./load_year.sh -s <idsite> -y <year> -t <type>
+
+type="R"
+
+while getopts "s:y:t:" opt; do
+  case $opt in
     s)
       idsite=$OPTARG
       ;;
     y)
       year=$OPTARG
+      ;;
+    t)
+      type=$OPTARG
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -18,10 +29,10 @@ while getopts "s:y:" opt; do
 done
 
 if [[ -z $idsite || -z $year ]]; then
-  echo "Usage: ./load_year.sh -s <idsite> -y <year>"
+  echo "Usage: ./load_year.sh -s <idsite> -y <year> -t <type>"
   exit 1
 fi
 
 for month in {1..12}; do
-  python3.10 s3parquet2elastic.py -s $idsite -y $year -m $month
+  python3.10 s3parquet2elastic.py -s $idsite -y $year -m $month -t $type
 done
