f5429523b7b762020bcf1c8483c15e5b1a41fdd8
Lautaro Matas <lmatas@gmail.com>
2024-05-29 12:09:29 +0200

    Create __init__.py

---
+++ stages/__init__.py
@@ -0,0 +1,7 @@
+from .aggbyitem_fstage import AggByItemFilterStage
+from .assets_fstage import AssetsFilterStage
+from .elastic_ostage import ElasticOutputStage
+from .metrics_fstage import MetricsFilterStage
+from .robots_fstage import RobotsFilterStage
+from .s3parquet_istage import S3ParquetInputStage
+from .identifier_fstage import IdentifierFilterStage
